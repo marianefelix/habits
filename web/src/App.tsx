157a255251119ebf26c40279aeeeffe69399d7a9
Mariane Felix <marianefelix59@gmail.com>
2023-01-18 19:36:26 -0300

    :sparkles: feat: create the header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
 import './styles/global.css';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  )
-}
 
-export default App
+export const App = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  )
+};
