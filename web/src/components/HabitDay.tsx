8ddcee392a051b9316c0ad7c0b1ed57f2e7a00a9
Mariane Felix <marianefelix59@gmail.com>
2023-01-19 11:17:22 -0300

    :sparkles: feat: add the day habits details and the progress
    - add the popover of the details about the habits of a day
    - create and add the progress bar component

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,26 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
+
 export const HabitDay = () => {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+        <Popover.Root>
+            <Popover.Trigger
+                className="
+                    w-10 h-10 bg-zinc-900 border-2 
+                    border-zinc-800 rounded-lg"
+            />
+            <Popover.Portal>
+                <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+                    <span className="font-semibold text-zinc-400">Terça-feira</span>
+                    <span className="mt-1 font-extrabold leading-tight text-3xl">19/01</span>
+                    <Popover.Arrow 
+                        height={8}
+                        width={16}
+                        className="fill-zinc-900" 
+                    />
+                    <ProgressBar progress={75} />
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
     );
 };
