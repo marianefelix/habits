8a69171bccfadcd88f32fe99ee4319649ead5831
Mariane Felix <marianefelix59@gmail.com>
2023-01-19 11:38:54 -0300

    :sparkles: feat: edit the style of days habits
    - edit the style (background e border color) of days squares according
    the habits completed percentage

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,13 +1,27 @@
 import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
-export const HabitDay = () => {
+interface HabitDayProps {
+    amount: number;
+    completed: number;
+}
+
+export const HabitDay = ({ amount, completed }: HabitDayProps) => {
+    const completedPercentage = Math.round((completed / amount) * 100);
+
     return (
         <Popover.Root>
             <Popover.Trigger
-                className="
-                    w-10 h-10 bg-zinc-900 border-2 
-                    border-zinc-800 rounded-lg"
+                className={clsx('w-10 h-10  border-2 rounded-lg', {
+                    'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+                    'bg-purple-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+                    'bg-purple-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+                    'bg-purple-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+                    'bg-purple-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+                    'bg-purple-500 border-violet-400': completedPercentage >= 80,
+
+                })}
             />
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
@@ -18,7 +32,7 @@ export const HabitDay = () => {
                         width={16}
                         className="fill-zinc-900" 
                     />
-                    <ProgressBar progress={75} />
+                    <ProgressBar progress={completedPercentage} />
                 </Popover.Content>
             </Popover.Portal>
         </Popover.Root>
